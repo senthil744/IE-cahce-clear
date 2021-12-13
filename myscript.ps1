f743ff81a6ac83e7c190ee5ab0ecbddd77f7e087
senthil744 <42367931+senthil744@users.noreply.github.com>
2021-12-13 13:26:26 +0530

    Update myscript.ps1

--- myscript.ps1
+++ myscript.ps1
@@ -1,6 +1,6 @@
-RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 255 (Deletes ALL History)
-RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 1 (Deletes History Only)
-RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 2 (Deletes Cookies Only)
-RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 8 (Deletes Temporary Internet Files Only)
-RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 16 (Deletes Form Data Only)
-RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 32 (Deletes Password History Only)
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 255 
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 1 
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 2 
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 8 
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 16 
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 32 
